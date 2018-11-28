2a53f90e9e652fe3e341168db1cd0712eb572ca6
ngaeng <gaeng@hm.edu>
2018-11-28 13:35:55 +0100

    Tests in richtigen Ordner verschoben

---
+++ src/test/main/java/tasche_packen/model/SubjectTest.java
@@ -0,0 +1,52 @@
+package main.java.tasche_packen.model;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SubjectTest {
+
+
+    @Test(timeout = 1_000)
+    public void removeItem() {
+        // arrange
+        Subject sut = new Subject("Operations Research");
+        sut.addItem(Item.Calculator);
+        sut.addItem(Item.Pad);
+        sut.addItem(Item.Notebook);
+        sut.removeItem(Item.Calculator);
+        List<Item> want = new ArrayList<Item>();
+
+        want.add(Item.Pad);
+        want.add(Item.Notebook);
+        // act
+        List<Item> have  = sut.getRequiredItems();
+        // assert
+        Assert.assertEquals(want, have);
+    }
+
+
+    @Test(timeout = 1_000)
+    public void removeNotExistingItem() {
+        // arrange
+        Subject sut = new Subject("Operations Research");
+        sut.addItem(Item.Calculator);
+        sut.addItem(Item.Pad);
+        sut.removeItem(Item.PencilCase);
+        List<Item> want = new ArrayList<Item>();
+        want.add(Item.Calculator);
+        want.add(Item.Pad);
+
+        // act
+        List<Item> have  = sut.getRequiredItems();
+        // assert
+        Assert.assertEquals(want, have);
+    }
+
+}
