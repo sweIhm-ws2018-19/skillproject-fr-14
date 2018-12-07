646fee909a68e801e908898be1a84eca70964316
Your Name <kkaiser@hm,edu>
2018-12-07 10:16:11 +0100

    Test für WelcomeIntent

--- src/test/java/tasche_packen/handlers/WelcomeIntentHandlerTest.java
+++ src/test/java/tasche_packen/handlers/WelcomeIntentHandlerTest.java
@@ -1,6 +1,8 @@
 package tasche_packen.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import tasche_packen.model.SubjectItemAssignment;
 
@@ -9,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 public class WelcomeIntentHandlerTest {
 
     @Test
-    public void welcomeIntentCanHandle() {
+    public void welcomeIntentHandlerCanHandle() {
         //HandlerInput Mock
         WelcomeIntentHandler sut = new WelcomeIntentHandler(new SubjectItemAssignment());
         HandlerInput inputMock = HandlerTest.mockHandlerInputWithString("WelcomeIntent");
@@ -17,4 +19,26 @@ public class WelcomeIntentHandlerTest {
         boolean want = true;
         assertEquals(want, have);
     }
+
+
+
+    @Test
+    public void welcomeIntentHandlerHandles() {
+        //HandlerInput Mock
+        WelcomeIntentHandler sut = new WelcomeIntentHandler(new SubjectItemAssignment());
+        final HandlerInput inputMock = HandlerTest.mockHandlerInput();
+        final Response response = sut.handle(inputMock).get();
+        final String have = response.toString();
+        Assert.assertTrue(have.contains( " Willst du heute alle Faecher besuchen ?"));
+    }
+
+
+
+
+
+
+
+
+
+
 }
